c7df73b39382e34fdfa657255b2a4ed3df639ff0
= <=>
2025-12-30 15:28:51 +0900

    refactor: Database 저장소를 ConcurrentHashMap으로 변경하여 스레드 안전성 확보

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Database {
-    private static Map<String, User> users = new HashMap<>();
+    private static Map<String, User> users = new java.util.concurrent.ConcurrentHashMap<>();
 
     public static void addUser(User user) {
         users.put(user.userId(), user);
