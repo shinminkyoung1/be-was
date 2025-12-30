0dafb44cf2b601f035ce04effd1f46dfbba1806f
apple <apple@appleui-MacBookPro-8.local>
2025-12-30 10:40:37 +0900

    feat: HTTP 요청 헤더 로그 출력 및 InputStream 파싱 로직 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +21,21 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // OutputStream을 문자열로 읽기 위한 보조 스트림 연결
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+
+            // HTTP 요청의 첫 번째 줄 읽음
+            String line = br.readLine();
+            if (line == null) {return;}
+            logger.debug("Request Line: {}", line);
+
+            // 나머지 헤더 정보 읽음
+            while (!line.equals("")) {
+                line = br.readLine();
+                if (line == null) break;
+                logger.debug("Header: {}", line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "<h1>Hello World</h1>".getBytes();
             response200Header(dos, body.length);
