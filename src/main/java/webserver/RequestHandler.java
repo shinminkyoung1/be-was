a17d5d5cce3d4b8869a319489e4bd5a74c67889b
shinminkyoung1 <dottiee0702@gmail.com>
2026-01-04 18:12:48 +0900

    refactor: HttpRequest 헤더 저장 로직 추가 및 Keep-Alive 기반 마련

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -22,30 +22,35 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // 요청 객체 생성
-            HttpRequest request = new HttpRequest(in);
+            while(true) {
+                // 요청 객체 생성
+                HttpRequest request = new HttpRequest(in);
 
-            // 응답 객체 생성
-            HttpResponse response = new HttpResponse(out);
+                // 더 이상 읽을 요청이 없으면 루프 종료
+                if (request.getPath() == null) break;
 
-            String path = request.getPath();
-            if (path == null) {
-                return;
+                // 응답 객체 생성
+                HttpResponse response = new HttpResponse(out);
+
+                // 경로에 맞는 핸들러 있는지 확인
+                Handler handler = RouteGuide.findHandler(request.getPath());
+
+                if (handler != null) {
+                    // 핸들러 있으면 해당 로직 수행
+                    handler.process(request, response);
+                } else {
+                    // 없으면 정적 파일 서빙
+                    response.fileResponse(resolveStaticPath(request.getPath()));
+                }
+
+                // Keep-Alive 여부에 따른 연결 종료 판단
+                if (!request.isKeepAlive()) {
+                    break; // Keep-Alive가 아니면 소켓 닫음
+                }
+                logger.debug("Keep-Alive: 유지 중");
             }
-
-            // 경로에 맞는 핸들러 있는지 확인
-            Handler handler = RouteGuide.findHandler(path);
-
-            if (handler != null) {
-                // 핸들러 있으면 해당 로직 수행
-                handler.process(request, response);
-            } else {
-                // 없으면 정적 파일 서빙
-                response.fileResponse(resolveStaticPath(path));
-            }
-
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            logger.error("소켓 통신 에러: {}", e.getMessage());
         }
     }
 
