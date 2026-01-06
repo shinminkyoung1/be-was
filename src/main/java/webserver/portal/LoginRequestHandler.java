e639625919e91d749b1626d242797f006b5fa93c
shinminkyoung1 <dottiee0702@gmail.com>
2026-01-06 17:59:35 +0900

    feat: 로그인 실패 이유 디버깅 및 처리 분리

--- src/main/java/webserver/portal/LoginRequestHandler.java
+++ src/main/java/webserver/portal/LoginRequestHandler.java
@@ -30,16 +30,18 @@ public class LoginRequestHandler implements Handler {
 
         User user = Database.findUserById(userId);
 
-        if (user != null && user.password().equals(password)) {
-            logger.debug("Login Success: {}", user.userId());
+        // 유저 없는 경우 로그인 실패
+        if (user != null) {
+            logger.debug("Login Failed: User ID '{}' not found in Database", userId);
+            response.sendRedirect(Config.LOGIN_PAGE);
+            return;
+        }
 
-            // 로그인 생성 시 세션 ID 생성
-            String sessionId = SessionManager.createSession(user);
-            // 쿠키로 세션 보냄
-            response.addHeader("Set-Cookie", SessionManager.getSessionCookieValue(sessionId));
-            response.sendRedirect(Config.MAIN_PAGE);
+        // 비밀번호 틀린 경우 로그인 실패
+        if (user.password().equals(password)) {
+            loginSuccess(user, response);
         } else {
-            logger.debug("Login Failed");
+            logger.debug("Login Failed: Wrong password for user '{}'", userId);
             response.sendRedirect(Config.LOGIN_PAGE);
         }
     }
