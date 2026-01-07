4ca00f358979439a9dd490ff89831b1bf9792482
shinminkyoung1 <dottiee0702@gmail.com>
2026-01-07 20:54:39 +0900

    fix: PageRender 내 중첩된 HTML 태그 오류 수정 및 링크 교정

--- src/main/java/webserver/engine/PageRender.java
+++ src/main/java/webserver/engine/PageRender.java
@@ -8,14 +8,20 @@ public class PageRender {
 
         if (loginUser != null) {
             // 로그인 상태: 글쓰기 + 이름(마이페이지) + 로그아웃
+            // [글쓰기] 버튼
             sb.append("<li class=\"header__menu__item\">")
-                    .append("<strong>").append(loginUser.name()).append("님</strong>")
-                    .append("</li>")
-                    .append("<li class=\"header__menu__item\">")
-                    .append("<a class=\"btn btn_ghost btn_size_s\" href=\"/mypage\">")
                     .append("<a class=\"btn btn_contained btn_size_s\" href=\"/article\">글쓰기</a>")
-                    .append("</a></li>")
-                    .append("<li class=\"header__menu__item\">")
+                    .append("</li>");
+
+            // [사용자 이름] 클릭 시 마이페이지 이동
+            sb.append("<li class=\"header__menu__item\">")
+                    .append("<a class=\"btn btn_ghost btn_size_s\" href=\"/mypage\">") // 여기서 링크 시작
+                    .append("<strong>").append(loginUser.name()).append("님</strong>")
+                    .append("</a>") // 링크 끝
+                    .append("</li>");
+
+            // [로그아웃] 버튼
+            sb.append("<li class=\"header__menu__item\">")
                     .append("<a class=\"btn btn_ghost btn_size_s\" href=\"/user/logout\">로그아웃</a>")
                     .append("</li>");
         } else {
