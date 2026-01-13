2cbce62602b50bb10ab2caa5a1d047a6d631af0a
shinminkyoung1 <dottiee0702@gmail.com>
2026-01-13 12:27:02 +0900

    feat: Jdbc 기반의 ArticleDao 구현 및 DB 연동

---
+++ src/main/java/db/ArticleDao.java
@@ -0,0 +1,52 @@
+package db;
+
+import model.Article;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleDao {
+
+    // 게시글 저장
+    public void insert(Article article) {
+        String sql = "INSERT INTO ARTICLE (writer, title, contents) VALUES (?, ?, ?)";
+
+        try (Connection connection = ConnectionManager.getConnection();
+        PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            pstmt.setString(1, article.writer());
+            pstmt.setString(2, article.title());
+            pstmt.setString(3, article.contents());
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to save article: " + e.getMessage());
+        }
+    }
+
+    // 게시글 조회
+    public List<Article> selectAll() {
+        String sql = "SELECT * FROM ARTICLE ORDER BY createdAt DESC";
+        List<Article> articles = new ArrayList<>();
+
+        try (Connection connection = ConnectionManager.getConnection();
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Article article = new Article(
+                        rs.getLong("id"),
+                        rs.getString("writer"),
+                        rs.getString("title"),
+                        rs.getString("contents"),
+                        rs.getTimestamp("createdAt").toLocalDateTime()
+                );
+                articles.add(article);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to retrieve article list: {}", e);
+        }
+        return articles;
+    }
+}
--- src/main/java/model/Article.java
+++ src/main/java/model/Article.java
@@ -3,4 +3,7 @@ package model;
 import java.time.LocalDateTime;
 
 public record Article (Long id,String writer, String title, String contents, LocalDateTime createdAt) {
+    public Article(String writer, String title, String contents) {
+        this(null, writer, title, contents, null);
+    }
 }
